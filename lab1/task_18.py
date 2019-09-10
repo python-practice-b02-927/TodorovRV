f7ad8d28fa5d98c3d7a3ae02695730cab33bd344
TodorovRV <55092584+TodorovRV@users.noreply.github.com>
2019-09-10 19:03:44 +0300

    Add files via upload

--- lab1/task_18.py
+++ lab1/task_18.py
@@ -5,6 +5,7 @@ from pyrob.api import *
 
 @task
 def task_8_28():
+    #Движение влево
     while not wall_is_on_the_left():
         move_left()
         if not wall_is_above():
@@ -13,6 +14,7 @@ def task_8_28():
         if not wall_is_beneath():
             move_down()
             break
+    #Движение вправо
     if wall_is_on_the_left():
         while not wall_is_on_the_right():
             move_right()
@@ -22,6 +24,7 @@ def task_8_28():
             if not wall_is_beneath():
                 move_down()
                 break
+    #Движение в верхний левый угол
     while not wall_is_on_the_left():
         move_left()
     while not wall_is_above():
