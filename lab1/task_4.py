aebd82ebf4e25fe39166715d131a9dd66bbb0e5f
TodorovRV <55092584+TodorovRV@users.noreply.github.com>
2019-09-13 16:36:57 +0300

    Add files via upload

--- lab1/task_4.py
+++ lab1/task_4.py
@@ -5,19 +5,14 @@ from pyrob.api import *
 
 @task
 def task_3_3():
-	while True:
 		if not wall_is_above():
 			move_up()
-			break
 		elif not wall_is_beneath():
 			move_down()
-			break
 		elif not wall_is_on_the_left():
 			move_left()
-			break
 		elif not wall_is_on_the_right():
 			move_right()
-			break
 
 
 if __name__ == '__main__':
