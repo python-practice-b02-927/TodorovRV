8764612111e03c5b0aeb886c175f39148602da76
TodorovRV <55092584+TodorovRV@users.noreply.github.com>
2019-09-13 16:54:43 +0300

    Add files via upload

--- lab1/task_14.py
+++ lab1/task_14.py
@@ -6,17 +6,12 @@ from pyrob.api import *
 @task
 def task_8_11():
     while not wall_is_on_the_right():
-        if not wall_is_above():
-            move_up()
-            fill_cell()
-            move_down()
-        if not wall_is_beneath():
-            move_down()
-            fill_cell()
-            move_up()
-        if wall_is_above() and wall_is_beneath():
-        	  fill_cell()
+        filling()
         move_right()
+    filling()
+
+
+def filling():
     if not wall_is_above():
         move_up()
         fill_cell()
@@ -27,6 +22,7 @@ def task_8_11():
         move_up()
     if wall_is_above() and wall_is_beneath():
         fill_cell()
+
 
 if __name__ == '__main__':
     run_tasks()
