3f02e709bbd5115c23d8ced5ff788e826444ccc6
TodorovRV <55092584+TodorovRV@users.noreply.github.com>
2019-09-15 01:01:54 +0300

    Add files via upload

--- lab2/13.py
+++ lab2/13.py
@@ -1,5 +1,6 @@
 import turtle as t
 
+
 def cir(n):
     t.begin_fill()
     t.circle(n)
@@ -7,40 +8,76 @@ def cir(n):
     t.color("black")
     t.circle(n)
 
+
 def duga(n):
     for i in range (100):
         t.forward(n)
         t.right(1.8)
 
-t.color("yellow")
-cir(100)
-t.penup()
-t.left(90)
-t.forward(170)
-t.left(90)
-t.forward(30)
-t.pendown()
-t.color("blue")
-cir(20)
-t.penup()
-t.backward(60)
-t.pendown()
-t.color("blue")
-cir(20)
-t.penup()
-t.forward(30)
-t.left(90)
-t.forward(60)
-t.width(3)
-t.pendown()
-t.forward(20)
-t.penup()
-t.forward(10)
-t.left(90)
-t.forward(60)
-t.right(90)
-t.color("red")
-t.pendown()
-duga(1.9)
+
+def eye():
+    t.color("blue")
+    cir(20)
+
+
+def face():
+    t.color("yellow")
+    cir(100)
+
+
+def moving1():
+    t.penup()
+    t.left(90)
+    t.forward(170)
+    t.left(90)
+    t.forward(30)
+    t.pendown()
+
+
+def eyes():
+    eye()
+    t.penup()
+    t.backward(60)
+    t.pendown()
+    eye()
+
+
+def moving2():
+    t.penup()
+    t.forward(30)
+    t.left(90)
+    t.forward(60)
+
+
+def nose():
+    t.width(3)
+    t.pendown()
+    t.forward(20)
+    t.width(1)
+
+
+def moving3():
+    t.penup()
+    t.forward(10)
+    t.left(90)
+    t.forward(60)
+    t.right(90)
+    t.pendown()
+
+
+def smile():
+    t.width(3)
+    t.color("red")
+    duga(1.9)
+    t.width(1)
+
+
+face()
+moving1()
+eyes()
+moving2()
+nose()
+moving3()
+smile()
 
 
