d277811790ef838bd847727e9b0d562bba6387f3
TodorovRV <todorov.rv@phystech.edu>
2019-09-24 23:18:01 +0300

    Пограмма теперь актуальна для любого расположения обьектов

--- lab3_2/animation.py
+++ lab3_2/animation.py
@@ -46,25 +46,81 @@ def obj(r):
         ob[i] = g.Circle(g.Point(x[i], y[i]), 2)
         ob[i].setFill('yellow')
         ob[i].draw(w)
-        a[i] = m.atan(((500 - x[i])/(500 - y[i])))
+        a[i] = abs(m.atan(((500 - x[i])/(500 - y[i]))))
 
     while True:
         for i in range(n):
-            dx = v[i] * m.sin(a[i]) * dt
-            dy = v[i] * m.cos(a[i]) * dt
-            if (x[i] - dx) < (500 - r*m.sin(a[i])):
-                x[i] += dx
-                y[i] += dy
-                ob[i].move(dx, dy)
-                accel = accel0 / ((500 - x[i]) ** 2 + (500 - y[i]) ** 2)
-                v[i] += accel * dt
-            else:
-                if st[i] == 1:
-                    continue
-                r += 20
-                star(r)
-                stop += 1
-                st[i] = 1
+            if x[i] < 500 and y[i] < 500:
+                dx = v[i] * m.sin(a[i]) * dt
+                dy = v[i] * m.cos(a[i]) * dt
+                if (x[i] + dx) < (500 - r * m.sin(a[i])):
+                    x[i] += dx
+                    y[i] += dy
+                    ob[i].move(dx, dy)
+                    accel = accel0 / ((500 - x[i]) ** 2 + (500 - y[i]) ** 2)
+                    v[i] += accel * dt
+                else:
+                    if st[i] == 1:
+                        continue
+                    r += 20
+                    star(r)
+                    stop += 1
+                    st[i] = 1
+                    ob[i].move(-dx, -dy)
+
+            if x[i] > 500 and y[i] > 500:
+                dx = v[i] * m.sin(a[i]) * dt
+                dy = v[i] * m.cos(a[i]) * dt
+                if (x[i] - dx) > (500 + r * m.sin(a[i])):
+                    x[i] -= dx
+                    y[i] -= dy
+                    ob[i].move(-dx, -dy)
+                    accel = accel0 / ((-500 + x[i]) ** 2 + (-500 + y[i]) ** 2)
+                    v[i] += accel * dt
+                else:
+                    if st[i] == 1:
+                        continue
+                    r += 20
+                    star(r)
+                    stop += 1
+                    st[i] = 1
+                    ob[i].move(dx, dy)
+
+            if x[i] < 500 and y[i] > 500:
+                dx = v[i] * m.sin(a[i]) * dt
+                dy = v[i] * m.cos(a[i]) * dt
+                if (x[i] + dx) < (500 - r * m.sin(a[i])):
+                    x[i] += dx
+                    y[i] -= dy
+                    ob[i].move(dx, -dy)
+                    accel = accel0 / ((-500 + x[i]) ** 2 + (-500 + y[i]) ** 2)
+                    v[i] += accel * dt
+                else:
+                    if st[i] == 1:
+                        continue
+                    r += 20
+                    star(r)
+                    stop += 1
+                    st[i] = 1
+                    ob[i].move(-dx, dy)
+
+            if x[i] > 500 and y[i] < 500:
+                dx = v[i] * m.sin(a[i]) * dt
+                dy = v[i] * m.cos(a[i]) * dt
+                if (x[i] - dx) > (500 + r * m.sin(a[i])):
+                    x[i] -= dx
+                    y[i] += dy
+                    ob[i].move(-dx, dy)
+                    accel = accel0 / ((-500 + x[i]) ** 2 + (-500 + y[i]) ** 2)
+                    v[i] += accel * dt
+                else:
+                    if st[i] == 1:
+                        continue
+                    r += 20
+                    star(r)
+                    stop += 1
+                    st[i] = 1
+                    ob[i].move(dx, -dy)
         g.time.sleep(0.0005)
         if stop == n:
             break
