224fc51abd4a1baecb34d8979bd3be7205bd334d
TodorovRV <todorov.rv@phystech.edu>
2019-10-07 16:24:54 +0300

    Added some descriptions and possibility to draw the star

--- lab3_2/animation.py
+++ lab3_2/animation.py
@@ -3,12 +3,17 @@ import math as m
 import random as ran
 
 
+r = 5
+# st - object status: 1 - object have fallen on star, 0 - object falling on star
 st = []
 x = []
 y = []
-a = []
-v = []
-ob = []
+# alpha - angle between direction to object and horizontal line
+alpha = []
+# vel = velocity
+vel = []
+object = []
+w = g.GraphWin("Model", 1000, 1000)
 
 
 def coords():
@@ -16,16 +21,18 @@ def coords():
     for i in range(n):
         x.append(0)
         y.append(0)
-        a.append(0)
-        v.append(0)
+        alpha.append(0)
+        vel.append(0)
         x[i] = int(ran.randint(1, 1000))
         y[i] = int(ran.randint(1, 1000))
-        ob.append(0)
+        object.append(0)
         st.append(0)
 
 
-def star():
-    pass
+def star(r):
+    star = g.Circle(g.Point(500, 500), r)
+    star.setFill('yellow')
+    star.draw(w)
 
 
 def object_movement():
@@ -38,7 +45,7 @@ def boom():
 
 def main():
     coords()
-    star()
+    star(r)
     object_movement()
     g.time.sleep(3)
     boom()
