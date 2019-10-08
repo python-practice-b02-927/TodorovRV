626a146a21cdc22d3d4a518fcd446260e3576e9f
TodorovRV <todorov.rv@phystech.edu>
2019-10-08 17:30:59 +0300

    Added second shape of target "square"

--- lab6/game.py
+++ lab6/game.py
@@ -2,23 +2,37 @@ from tkinter import *
 from random import randrange as rnd, choice
 import time
 import math as m
+
 root = Tk()
 root.geometry('800x600')
 
 ball = []
-canv = Canvas(root,bg='white')
-canv.pack(fill=BOTH,expand=1)
+canv = Canvas(root, bg='white')
+canv.pack(fill=BOTH, expand=1)
 l = Label(bg='black', fg='white', width=20)
 score = 0
 l.pack()
 
-colors = ['red','orange','yellow','green','blue']
+colors = ['red', 'orange', 'yellow', 'green', 'blue']
+shapes = ['square', 'circle']
+
+
 def new_ball():
     global ball
-    ball.append({'id' : 0, 'x' : rnd(100,700), 'y' : rnd(100,500), 'r' : rnd(30,50), 'vx' : rnd(-4, 4), 'vy' : rnd(-4, 4)})
-    ball[len(ball) - 1]['id'] = canv.create_oval(ball[len(ball) - 1]['x'] - ball[len(ball) - 1 ]['r'],ball[len(ball) - 1]['y'] -
-                                           ball[len(ball) - 1]['r'],ball[len(ball) - 1]['x'] + ball[len(ball) - 1]['r'],
-                                           ball[len(ball) - 1]['y'] + ball[len(ball) - 1]['r'],fill = choice(colors), width=0)
+    ball.append({'id': 0, 'x': rnd(100, 700), 'y': rnd(100, 500), 'r': rnd(30, 50), 'vx': rnd(-4, 4),
+                 'vy': rnd(-4, 4), 'color': choice(colors), 'shape': choice(shapes)})
+    if ball[len(ball) - 1]['shape'] == 'circle':
+        ball[len(ball) - 1]['id'] = canv.create_oval(ball[len(ball) - 1]['x'] - ball[len(ball) - 1]['r'],
+                                                     ball[len(ball) - 1]['y'] - ball[len(ball) - 1]['r'],
+                                                     ball[len(ball) - 1]['x'] + ball[len(ball) - 1]['r'],
+                                                     ball[len(ball) - 1]['y'] + ball[len(ball) - 1]['r'],
+                                                     fill=ball[len(ball) - 1]['color'], width=0)
+    if ball[len(ball) - 1]['shape'] == 'square':
+        ball[len(ball) - 1]['id'] = canv.create_rectangle(ball[len(ball) - 1]['x'] - ball[len(ball) - 1]['r'],
+                                                        ball[len(ball) - 1]['y'] - ball[len(ball) - 1]['r'],
+                                                        ball[len(ball) - 1]['x'] + ball[len(ball) - 1]['r'],
+                                                        ball[len(ball) - 1]['y'] + ball[len(ball) - 1]['r'],
+                                                        fill=ball[len(ball) - 1]['color'], width=0)
     root.after(450, new_ball)
     if len(ball) > 4:
         canv.delete(ball[0]['id'])
@@ -30,12 +44,20 @@ def click(event):
     x0 = event.x
     y0 = event.y
     for i in range(len(ball) - 1):
-        if (m.fabs(x0 - ball[i]['x']) < ball[i]['r']) & (m.fabs(y0 - ball[i]['y']) < ball[i]['r']):
-            score += m.floor(ball[i]['r'] / (m.floor(m.fabs(x0 - ball[i]['x'])**2) + 1 +
-                                             m.floor((m.fabs(y0 - ball[i]['y'])) ** 2)) * 100)
-            l['text'] = 'Score: ' + str(score)
-            canv.delete(ball[i]['id'])
-            del ball[i]
+        if ball[i]['shape'] == 'circle':
+            if m.fabs(x0 - ball[i]['x']) ** 2 + m.fabs(y0 - ball[i]['y']) ** 2 < ball[i]['r'] ** 2:
+                score += m.floor(ball[i]['r'] / (m.floor(m.fabs(x0 - ball[i]['x']) ** 2) + 1 +
+                                                 m.floor((m.fabs(y0 - ball[i]['y'])) ** 2)) * 100)
+                l['text'] = 'Score: ' + str(score)
+                canv.delete(ball[i]['id'])
+                del ball[i]
+        if ball[i]['shape'] == 'square':
+            if (m.fabs(x0 - ball[i]['x']) < ball[i]['r']) & (m.fabs(y0 - ball[i]['y']) < ball[i]['r']):
+                score += m.floor(ball[i]['r'] / (m.floor(m.fabs(x0 - ball[i]['x']) ** 2) + 1 +
+                                                 m.floor((m.fabs(y0 - ball[i]['y'])) ** 2)) * 100)
+                l['text'] = 'Score: ' + str(score)
+                canv.delete(ball[i]['id'])
+                del ball[i]
 
 
 def movement():
@@ -43,15 +65,21 @@ def movement():
     for i in range(len(ball) - 1):
         if (ball[i]['x'] + ball[i]['vx'] > 800) | (ball[i]['x'] + ball[i]['vx'] < 0):
             ball[i]['vx'] = -ball[i]['vx']
-        if(ball[i]['y'] + ball[i]['vy'] > 600) | (ball[i]['y'] + ball[i]['vy'] < 0):
+        if (ball[i]['y'] + ball[i]['vy'] > 600) | (ball[i]['y'] + ball[i]['vy'] < 0):
             ball[i]['vy'] = -ball[i]['vy']
         ball[i]['x'] += ball[i]['vx']
         ball[i]['y'] += ball[i]['vy']
         canv.delete(ball[i]['id'])
-        ball[i]['id'] = canv.create_oval(ball[i]['x'] - ball[i]['r'], ball[i]['y'] - ball[i]['r'],
-                                                     ball[i]['x'] + ball[i]['r'],ball[i]['y'] + ball[i]['r'],
-                                                     fill=choice(colors), width=0)
+        if ball[i]['shape'] == 'circle':
+            ball[i]['id'] = canv.create_oval(ball[i]['x'] - ball[i]['r'], ball[i]['y'] - ball[i]['r'],
+                                             ball[i]['x'] + ball[i]['r'], ball[i]['y'] + ball[i]['r'],
+                                             fill=ball[i]['color'], width=0)
+        if ball[i]['shape'] == 'square':
+            ball[i]['id'] = canv.create_rectangle(ball[i]['x'] - ball[i]['r'], ball[i]['y'] - ball[i]['r'],
+                                             ball[i]['x'] + ball[i]['r'], ball[i]['y'] + ball[i]['r'],
+                                             fill=ball[i]['color'], width=0)
     root.after(5, movement)
+
 
 new_ball()
 movement()
