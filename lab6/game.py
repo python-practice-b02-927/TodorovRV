8ab2f920461b37eee00f560f922d36bf82f38f0d
TodorovRV <todorov.rv@phystech.edu>
2019-10-10 20:06:25 +0300

    Added scoretable file

--- lab6/game.py
+++ lab6/game.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from random import randrange as rnd, choice
-import time
+import json
 import math as m
 
 root = Tk()
@@ -11,6 +11,7 @@ canv = Canvas(root, bg='white')
 canv.pack(fill=BOTH, expand=1)
 l = Label(bg='black', fg='white', width=20)
 score = 0
+ball_number = 0
 l.pack()
 
 colors = ['red', 'orange', 'yellow', 'green', 'blue']
@@ -18,26 +19,32 @@ shapes = ['square', 'circle']
 
 
 def new_ball():
-    global ball
-    ball.append({'id': 0, 'x': rnd(100, 700), 'y': rnd(100, 500), 'r': rnd(30, 50), 'vx': rnd(-4, 4),
-                 'vy': rnd(-4, 4), 'color': choice(colors), 'shape': choice(shapes)})
-    if ball[len(ball) - 1]['shape'] == 'circle':
-        ball[len(ball) - 1]['id'] = canv.create_oval(ball[len(ball) - 1]['x'] - ball[len(ball) - 1]['r'],
-                                                     ball[len(ball) - 1]['y'] - ball[len(ball) - 1]['r'],
-                                                     ball[len(ball) - 1]['x'] + ball[len(ball) - 1]['r'],
-                                                     ball[len(ball) - 1]['y'] + ball[len(ball) - 1]['r'],
-                                                     fill=ball[len(ball) - 1]['color'], width=0)
-    if ball[len(ball) - 1]['shape'] == 'square':
-        ball[len(ball) - 1]['id'] = canv.create_rectangle(ball[len(ball) - 1]['x'] - ball[len(ball) - 1]['r'],
-                                                        ball[len(ball) - 1]['y'] - ball[len(ball) - 1]['r'],
-                                                        ball[len(ball) - 1]['x'] + ball[len(ball) - 1]['r'],
-                                                        ball[len(ball) - 1]['y'] + ball[len(ball) - 1]['r'],
-                                                        fill=ball[len(ball) - 1]['color'], width=0)
-    root.after(450, new_ball)
-    if len(ball) > 4:
-        canv.delete(ball[0]['id'])
-        del ball[0]
-
+    try:
+        global ball, ball_number
+        ball.append({'id': 0, 'x': rnd(100, 700), 'y': rnd(100, 500), 'r': rnd(30, 50), 'vx': rnd(-4, 4),
+                     'vy': rnd(-4, 4), 'color': choice(colors), 'shape': choice(shapes)})
+        if ball[len(ball) - 1]['shape'] == 'circle':
+            ball[len(ball) - 1]['id'] = canv.create_oval(ball[len(ball) - 1]['x'] - ball[len(ball) - 1]['r'],
+                                                         ball[len(ball) - 1]['y'] - ball[len(ball) - 1]['r'],
+                                                         ball[len(ball) - 1]['x'] + ball[len(ball) - 1]['r'],
+                                                         ball[len(ball) - 1]['y'] + ball[len(ball) - 1]['r'],
+                                                         fill=ball[len(ball) - 1]['color'], width=0)
+        if ball[len(ball) - 1]['shape'] == 'square':
+            ball[len(ball) - 1]['id'] = canv.create_rectangle(ball[len(ball) - 1]['x'] - ball[len(ball) - 1]['r'],
+                                                            ball[len(ball) - 1]['y'] - ball[len(ball) - 1]['r'],
+                                                            ball[len(ball) - 1]['x'] + ball[len(ball) - 1]['r'],
+                                                            ball[len(ball) - 1]['y'] + ball[len(ball) - 1]['r'],
+                                                            fill=ball[len(ball) - 1]['color'], width=0)
+        if ball_number < 100:
+            root.after(450, new_ball)
+        else:
+            root.destroy()
+        if len(ball) > 4:
+            canv.delete(ball[0]['id'])
+            del ball[0]
+        ball_number += 1
+    except TclError:
+        pass
 
 def click(event):
     global ball, score
@@ -66,28 +73,48 @@ def click(event):
         i += 1
 
 
+
 def movement():
-    global ball
-    for i in range(len(ball)):
-        if (ball[i]['x'] + ball[i]['vx'] > 800) | (ball[i]['x'] + ball[i]['vx'] < 0):
-            ball[i]['vx'] = -ball[i]['vx']
-        if (ball[i]['y'] + ball[i]['vy'] > 600) | (ball[i]['y'] + ball[i]['vy'] < 0):
-            ball[i]['vy'] = -ball[i]['vy']
-        ball[i]['x'] += ball[i]['vx']
-        ball[i]['y'] += ball[i]['vy']
-        canv.delete(ball[i]['id'])
-        if ball[i]['shape'] == 'circle':
-            ball[i]['id'] = canv.create_oval(ball[i]['x'] - ball[i]['r'], ball[i]['y'] - ball[i]['r'],
-                                             ball[i]['x'] + ball[i]['r'], ball[i]['y'] + ball[i]['r'],
-                                             fill=ball[i]['color'], width=0)
-        if ball[i]['shape'] == 'square':
-            ball[i]['id'] = canv.create_rectangle(ball[i]['x'] - ball[i]['r'], ball[i]['y'] - ball[i]['r'],
-                                             ball[i]['x'] + ball[i]['r'], ball[i]['y'] + ball[i]['r'],
-                                             fill=ball[i]['color'], width=0)
-    root.after(5, movement)
+    try:
+        global ball, ball_number
+        for i in range(len(ball)):
+            if (ball[i]['x'] + ball[i]['vx'] > 800) | (ball[i]['x'] + ball[i]['vx'] < 0):
+                ball[i]['vx'] = -ball[i]['vx']
+            if (ball[i]['y'] + ball[i]['vy'] > 600) | (ball[i]['y'] + ball[i]['vy'] < 0):
+                ball[i]['vy'] = -ball[i]['vy']
+            ball[i]['x'] += ball[i]['vx']
+            ball[i]['y'] += ball[i]['vy']
+            canv.delete(ball[i]['id'])
+            if ball[i]['shape'] == 'circle':
+                ball[i]['id'] = canv.create_oval(ball[i]['x'] - ball[i]['r'], ball[i]['y'] - ball[i]['r'],
+                                                 ball[i]['x'] + ball[i]['r'], ball[i]['y'] + ball[i]['r'],
+                                                 fill=ball[i]['color'], width=0)
+            if ball[i]['shape'] == 'square':
+                ball[i]['id'] = canv.create_rectangle(ball[i]['x'] - ball[i]['r'], ball[i]['y'] - ball[i]['r'],
+                                                 ball[i]['x'] + ball[i]['r'], ball[i]['y'] + ball[i]['r'],
+                                                 fill=ball[i]['color'], width=0)
+        if ball_number < 100:
+            root.after(5, movement)
+    except TclError:
+        pass
+
+
+def results():
+    nick = input("Enter you nickname\n")
+    with open('Scoretable.json') as f:
+        scoretable = json.load(f)
+    if nick in scoretable:
+        if scoretable[nick] < score:
+            scoretable[nick] = score
+    else:
+        scoretable[nick] = score
+    with open('Scoretable.json', 'w') as f:
+        json.dump(scoretable, f, indent=1)
 
 
 new_ball()
 movement()
 canv.bind('<Button-1>', click)
 mainloop()
+results()
+
